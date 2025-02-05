4e0d2c070188a71ed0ea523d032b50b7be784965
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2025-02-05 22:24:03 +0530

    Delete FE4_module-main directory

--- FE4_module-main/src/components/redux/store.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { applyMiddleware, createStore } from 'redux'
-import { thunk } from 'redux-thunk'
-import reducer from './reducer'
-
-const store = createStore(reducer, applyMiddleware(thunk))
-
-export default store
